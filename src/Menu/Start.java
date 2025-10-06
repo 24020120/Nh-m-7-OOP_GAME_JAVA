14b215c84488915d2f3501491cc10c82cac1a060
Syrex310 <duongrobloxvn@gmail.com>
2025-10-06 11:30:54 +0700

    added start button, menubar and gameloop

--- src/Menu/Start.java
+++ src/Menu/Start.java
@@ -1,4 +1,13 @@
 package Menu;
+import javax.swing.*;
+import javax.swing.plaf.ColorUIResource;
 
 public class Start {
+    public static JButton CreateStartButton() {
+        JButton startButton = new JButton("START");
+        startButton.setSize(50, 20);
+        startButton.setVisible(true);
+        startButton.setBackground(new ColorUIResource(0, 128, 0));
+        return startButton;
+    }
 }
